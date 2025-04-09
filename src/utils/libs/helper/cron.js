7d127f56f984e0f9030366a43a21e7d54d5c1e56
muqadasqk <zainaliqk@gmail.com>
2025-04-09 17:30:36 +0500

    Make modifications in the main auth controller logics

--- src/utils/libs/helper/cron.js
+++ src/utils/libs/helper/cron.js
@@ -9,14 +9,14 @@ cron.schedule('0 0 * * *', async () => {
     // check and delete expired projects and their proposal files
     const expiredProjects = await Project.find({ expiresAt: { $lte: new Date() } });
     for (const project of expiredProjects) {
-        if (project.proposal) file.delete(project.proposal);
+        if (project?.proposal) file.delete(project.proposal);
         await project.deleteOne();
     }
 
     // check and delete expired presentations and their resource files
     const expiredPresentations = await Presentation.find({ expiresAt: { $lte: new Date() } });
     for (const presentation of expiredPresentations) {
-        if (presentation.resource) file.delete(presentation.resource);
+        if (presentation?.resource) file.delete(presentation.resource);
         await presentation.deleteOne();
     }
 });
